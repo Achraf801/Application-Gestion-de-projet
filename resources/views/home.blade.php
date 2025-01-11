72f4ef01108688120e15c34cca2dd29192efb475
Achraf801 <achrafabd74@gmail.com>
2025-01-11 20:15:46 +0100

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-row  items-center ">
         <ul class="flex flex-row justify-between items-center mb-3 md:mb-0">
             <li class="md:mr-5 py-2 md:py-0"><a href="#" class="hover:text-gray-400">Mes tâches </a></li>
-               <li class="md:mr-5 py-2 md:py-0"><a href="#" class="hover:text-gray-400"> Mes taches creer</a></li>
+               <li class="md:mr-5 py-2 md:py-0"><a href="#" class="hover:text-gray-400"> Mes taches creer par moi </a></li>
             <li class="md:mr-5 py-2 md:py-0"><a href="#" class="hover:text-gray-400">Gestion des users</a></li>
             <li class="md:mr-5 py-2 md:py-0"><a href="#" class="hover:text-gray-400">Nouvelle tâche</a></li>
        </ul>
